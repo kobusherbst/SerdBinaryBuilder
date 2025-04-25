712ce5cb56b2d5bf8428da829f17cec46f338b06
Kobus Herbst <kobush@gmail.com>
2025-04-25 23:23:54 +0200

    Refactor build script: streamline setup process and remove Windows-specific cross-file handling

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -1,11 +1,14 @@
-using BinaryBuilder, BinaryBuilderBase
+using BinaryBuilder
+using BinaryBuilderBase
 
 name = "Serd"
 version = v"0.32.4"
 
 sources = [
-    ArchiveSource("https://download.drobilla.net/serd-0.32.4.tar.xz",
-                  "cbefb569e8db686be8c69cb3866a9538c7cb055e8f24217dd6a4471effa7d349")
+    ArchiveSource(
+        "https://download.drobilla.net/serd-0.32.4.tar.xz",
+        "cbefb569e8db686be8c69cb3866a9538c7cb055e8f24217dd6a4471effa7d349"
+    )
 ]
 
 script = raw"""
@@ -15,42 +18,16 @@ export PATH="$meson_path:$ninja_path:$PATH"
 
 mkdir build && cd build
 
-if [[ "${target}" == *w64-mingw32* ]]; then
-    echo "Detected Windows target, generating custom cross file..."
-
-    cat > cross_file_windows.txt <<EOF
-[binaries]
-c = '/opt/x86_64-w64-mingw32/bin/x86_64-w64-mingw32-gcc'
-cpp = '/opt/x86_64-w64-mingw32/bin/x86_64-w64-mingw32-g++'
-ar = '/opt/x86_64-w64-mingw32/bin/x86_64-w64-mingw32-ar'
-strip = '/opt/x86_64-w64-mingw32/bin/x86_64-w64-mingw32-strip'
-pkgconfig = 'pkg-config'
-
-[host_machine]
-system = 'windows'
-cpu_family = 'x86_64'
-cpu = 'x86_64'
-endian = 'little'
-EOF
-
-    meson setup --prefix=${prefix} --buildtype=release --default-library=shared --cross-file=cross_file_windows.txt ${WORKSPACE}/srcdir/serd-0.32.4
-else
-    echo "Non-Windows target detected, using default MESON_TARGET_TOOLCHAIN..."
-
-    meson setup --prefix=${prefix} --buildtype=release --default-library=both --cross-file=${MESON_TARGET_TOOLCHAIN} ${WORKSPACE}/srcdir/serd-0.32.4
-fi
+meson setup --prefix=${prefix} --buildtype=release --default-library=shared ${WORKSPACE}/srcdir/serd-0.32.4
 
 ninja -j${nproc}
+ninja install
 
-# Manually copy built libraries
-mkdir -p ${prefix}/lib
-mkdir -p ${prefix}/bin
-
-find . -name '*.so*' -exec cp {} ${prefix}/lib/ \;
-find . -name '*.dll' -exec cp {} ${prefix}/bin/ \;
-
-# Install license
 install -D -m644 ${WORKSPACE}/srcdir/serd-0.32.4/COPYING ${prefix}/share/licenses/Serd/COPYING
+
+# Show installed files
+echo "=== INSTALLED FILES ==="
+find ${prefix} || true
 """
 
 platforms = [
@@ -61,8 +38,7 @@ platforms = [
 dependencies = []
 
 products = [
-    LibraryProduct(["libserd-0"], :serd),
-    FileProduct(["libserd-0.dll"], :serd_dll)
+    LibraryProduct(["libserd-0"], :serd)
 ]
 
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")

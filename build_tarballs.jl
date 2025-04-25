21a971d8458e4236504410ff88355d3ad29f08a7
Kobus Herbst <kobush@gmail.com>
2025-04-25 14:08:15 +0200

    Add julia_compat = 1.7

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -35,4 +35,4 @@ products = [
 
 dependencies = []
 
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies, julia_compat="1.7")
